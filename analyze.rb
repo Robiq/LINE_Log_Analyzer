dcc06c7b0d40567d407670b8ecfd299c95712046
Robin Lunde <robin@wub.no>
2019-09-01 16:20:15 +0900

    Word analysis and print complete

--- analyze.rb
+++ analyze.rb
@@ -73,7 +73,7 @@ def add_word_data(word, words)
 
 	for w in word
 		# todo - remove filler word. (the, this, that...etc)
-		ignore_words = ["to","of","in","for","on","with","at","by","from","up","about","into","over","after","Others","the","and","a","that","I","it","not","he","as","you","this","but","his","they","her","she","or","an","will","my","one","all","would","there","their"]
+		ignore_words = ["to","of","in","for","on","with","at","by","from","up","about","into","over","after","others","the","and","a","that","I","it","not","he","as","you","this","but","his","they","her","she","or","an","will","my","one","all","would","there","their", "i", "am", "ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than", "ok"]
 		if ignore_words.include? w
 			return
 		end
@@ -86,10 +86,24 @@ def add_word_data(word, words)
 	end
 end
 
+def extract_word_stats(words, result)
+	result[:uniq] = words.length
+	sorted = words.each_with_object({}) { |(k,v),g| (g[v] ||= []) << k }
+	high = sorted.max_by{|k,v| k}[0]
+	#puts sorted
+	#puts high
+	#puts sorted[1]
+	result[:most_nr] = high
+	result[:most] = sorted[high]
+	result[:least] = sorted[1]
+	result[:sorted] = sorted
+	#puts result[:least]
+end
+
 def analyze_pr_sender(data)
 	#Data
 	# Name, [ent, ent]
-	# ent = [datetime, msg, type]
+	# ent = {datetime, msg, type}
 	
 	#Res
 	# res[usr] = {msg_stats: {msg_nr, stamp_nr, photo_nr}, word_stats: {uniq, most, least}, reply_time}
@@ -99,7 +113,8 @@ def analyze_pr_sender(data)
 	data.each_key do |user|
 		#puts "Key: " + user
 		#puts data[user]
-		res[user] = {msg_stats: {text_nr:0, stamp_nr:0, photo_nr:0}, word_stats: {uniq:0, most:0, least:0}, reply_time:nil}
+		wordlist = {}
+		res[user] = {msg_stats: {text_nr:0, stamp_nr:0, photo_nr:0}, word_stats: {uniq:0, most:[], most_nr:0, least:[], sorted:nil}, reply_time:nil}
 		data[user].each do |msg_data|
 			#puts msg_data
 			if msg_data[:type] == "p"
@@ -110,14 +125,29 @@ def analyze_pr_sender(data)
 				res[user][:msg_stats][:text_nr] += 1
 			end
 			
-						
+			# word analysis per user
+			if msg_data[:type] == "m"
+				for word in msg_data[:msg].split
+					add_word_data(word, wordlist)
+				end
+			end
 
 		end
-		puts "Stats for user: " + user
-		puts "Stamps: " + res[user][:msg_stats][:stamp_nr].to_s
-		puts "Photos: " + res[user][:msg_stats][:photo_nr].to_s
-		puts "Text: " + res[user][:msg_stats][:text_nr].to_s
-		puts "Total messages: " + (res[user][:msg_stats][:stamp_nr] + res[user][:msg_stats][:photo_nr] + res[user][:msg_stats][:text_nr]).to_s + "\n"
+
+		extract_word_stats(wordlist, res[user][:word_stats])
+
+		# User stats
+		puts "------------------ User: " + user + " --------------------------"
+		puts "******** Word stats ********"
+		puts "Unique words: " + res[user][:word_stats][:uniq].to_s
+		puts "Most used words were used " + res[user][:word_stats][:most_nr].to_s + " times."
+		puts "Most used words: " + res[user][:word_stats][:most].join(", ")
+		#puts "Words used only once: " + res[user][:word_stats][:least].join(", ")
+		#puts "******** Message stats ********"
+		#puts "Stamps: " + res[user][:msg_stats][:stamp_nr].to_s
+		#puts "Photos: " + res[user][:msg_stats][:photo_nr].to_s
+		#puts "Text: " + res[user][:msg_stats][:text_nr].to_s
+		#puts "Total messages: " + (res[user][:msg_stats][:stamp_nr] + res[user][:msg_stats][:photo_nr] + res[user][:msg_stats][:text_nr]).to_s + "\n"
 	end
 	#for user in data
 	#	puts user
@@ -158,6 +188,7 @@ def analyze_lines(chat)
 			end
 		end
 
+
 		if ent.LINE.type == "s"
 			stickers += 1
 		elsif ent.LINE.type == "p"
@@ -171,6 +202,10 @@ def analyze_lines(chat)
 			analyze_reply_time(sender, datetime)
 		end
 	end
+	
+	word_stats = {uniq:0, most:[], most_nr:0, least:[], sorted:nil}
+	#Extract data for all users
+	extract_word_stats(words, word_stats)
 
 	analyze_pr_sender(sender_message)
 
